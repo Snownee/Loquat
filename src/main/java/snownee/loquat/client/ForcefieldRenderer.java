de5d12224f9154a77a7f3f82b1a09ab438e21a75
Snownee <i@snownee.com>
2023-06-18 23:41:21 +0800

    feat: implement KubeJS WithPersistentData for Area

---
+++ src/main/java/snownee/loquat/client/ForcefieldRenderer.java
@@ -0,0 +1,12 @@
+package snownee.loquat.client;
+
+import net.minecraft.resources.ResourceLocation;
+
+public class ForcefieldRenderer {
+	private static final ResourceLocation FORCEFIELD_LOCATION = new ResourceLocation("textures/misc/forcefield.png");
+
+	public static void render() {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ src/main/java/snownee/loquat/mixin/MixinPlugin.java
@@ -0,0 +1,55 @@
+package snownee.loquat.mixin;
+
+import java.util.List;
+import java.util.Set;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import net.fabricmc.loader.api.FabricLoader;
+
+public class MixinPlugin implements IMixinConfigPlugin {
+
+	private boolean hasKubeJS;
+
+	private static boolean hasMod(String modid) {
+		return FabricLoader.getInstance().isModLoaded(modid);
+	}
+
+	@Override
+	public void onLoad(String mixinPackage) {
+		hasKubeJS = hasMod("kubejs");
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		if (mixinClassName.startsWith("snownee.loquat.mixin.kubejs.")) {
+			return hasKubeJS;
+		}
+		return true;
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+	}
+
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
+
+}
