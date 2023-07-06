7548c2bbea614f95d9fa272bbfe28449effe8be1
Snownee <i@snownee.com>
2023-07-06 13:47:29 +0800

    target KubeJS 6.1

--- src/main/java/snownee/loquat/compat/kubejs/LoquatKubeJSPlugin.java
+++ src/main/java/snownee/loquat/compat/kubejs/LoquatKubeJSPlugin.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.script.BindingsEvent;
+import dev.latvian.mods.kubejs.script.ScriptType;
 import snownee.loquat.Loquat;
 import snownee.loquat.core.AreaManager;
 import snownee.loquat.placement.LoquatPlacements;
@@ -17,10 +18,12 @@ public class LoquatKubeJSPlugin extends KubeJSPlugin {
 	public void init() {
 		Loquat.LOGGER.info("KubeJS detected, loading Loquat KubeJS plugin");
 		CommonProxy.registerPlayerEnterAreaListener((player, area) -> {
-			LoquatKubeJSEvents.AREA_ENTERED.post(Objects.requireNonNull(area.getUuid()).toString(), new PlayerAreaEventJS(player, area));
+			if (LoquatKubeJSEvents.AREA_ENTERED.hasListeners())
+				LoquatKubeJSEvents.AREA_ENTERED.post(ScriptType.SERVER, Objects.requireNonNull(area.getUuid()).toString(), new PlayerAreaEventJS(player, area));
 		});
 		CommonProxy.registerPlayerLeaveAreaListener((player, area) -> {
-			LoquatKubeJSEvents.AREA_LEFT.post(Objects.requireNonNull(area.getUuid()).toString(), new PlayerAreaEventJS(player, area));
+			if (LoquatKubeJSEvents.AREA_LEFT.hasListeners())
+				LoquatKubeJSEvents.AREA_LEFT.post(ScriptType.SERVER, Objects.requireNonNull(area.getUuid()).toString(), new PlayerAreaEventJS(player, area));
 		});
 	}
 
